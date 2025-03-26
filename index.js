fa8b5042d674831be426d0b01196757e163cd049
Tojo6450 <124512052+Tojo6450@users.noreply.github.com>
2025-03-26 13:31:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+//web server
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1> I am Supriyo Ghorui.</h1>");
+    res.end();
+  }
+
+  if (req.url === "/source-code") {
+    res.write(
+      "Hello, I am learning and my source code"
+    );
+    res.end();
+  }
+
+  if (req.url === "/contact") {
+    res.setHeader("Content-Type", "text/plain");
+    res.write("This is my Contact no.");
+    res.end();
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`🔥 Listening on PORT ${PORT}`);
+});
